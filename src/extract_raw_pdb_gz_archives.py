15b1d46b7a16362520e011b955524ad783b9628b
Alex Morehead <alex.morehead@gmail.com>
2020-10-13 23:25:58 -0500

    Add check to ensure that only GZ archives are inspected for extraction

--- src/extract_raw_pdb_gz_archives.py
+++ src/extract_raw_pdb_gz_archives.py
@@ -20,12 +20,13 @@ def main(pdb_gz_data):
     raw_pdb_list = os.listdir(data_dir)
     for pdb_dir in raw_pdb_list:
         for pdb_gz in tqdm(os.listdir(data_dir + pdb_dir)):
-            _, ext = os.path.splitext(pdb_gz)
-            gzip_dir = data_dir + pdb_dir + '/' + pdb_gz
-            extract_dir = data_dir + pdb_dir + '/' + _
-            with gzip.open(gzip_dir, 'rb') as f_in:
-                with open(extract_dir, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
+            if 'gz' in pdb_gz:
+                _, ext = os.path.splitext(pdb_gz)
+                gzip_dir = data_dir + pdb_dir + '/' + pdb_gz
+                extract_dir = data_dir + pdb_dir + '/' + _
+                with gzip.open(gzip_dir, 'rb') as f_in:
+                    with open(extract_dir, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
 
 
 if __name__ == '__main__':
