06b4eef935e80c238f0f606f944dd698bc03b5b9
Alex Morehead <alex.morehead@gmail.com>
2020-10-15 13:09:49 -0500

    Fix GZ file name extension check

--- src/extract_raw_pdb_gz_archives.py
+++ src/extract_raw_pdb_gz_archives.py
@@ -20,7 +20,7 @@ def main(gz_data_dir):
     raw_pdb_list = os.listdir(data_dir)
     for pdb_dir in raw_pdb_list:
         for pdb_gz in tqdm(os.listdir(data_dir + pdb_dir)):
-            if 'gz' in pdb_gz:
+            if pdb_gz.endswith('.gz'):
                 _, ext = os.path.splitext(pdb_gz)
                 gzip_dir = data_dir + pdb_dir + '/' + pdb_gz
                 extract_dir = data_dir + pdb_dir + '/' + _
