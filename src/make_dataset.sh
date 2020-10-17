1acfa3c8df1035774947aade37819fe7bbb83f74
Alex C Morehead <acmwhb@lewis4-r7425-htc5-node835.cobalt.lan>
2020-10-17 14:48:04 -0500

    Create SBATCH script for compiling DIPS

---
+++ src/make_dataset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+####################### Batch Headers #########################
+#SBATCH -p Lewis
+#SBATCH -J make_dips_dataset
+#SBATCH -t 0-02:00
+#SBATCH -N 1
+#SBATCH -n 24
+###############################################################
+
+export MYLOCAL=/home/$USER/data/DIPS/DIPS/
+source $MYLOCAL/venv/bin/activate
+
+python $MYLOCAL/src/make_dataset.py $MYLOCAL/data/DIPS/raw/pdb $MYLOCAL/data/DIPS/interim --num_cpus 24
