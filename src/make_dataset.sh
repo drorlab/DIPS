84c80078f2b6a53789b809ae54fa9cc46a795f38
Alex Morehead <alex.morehead@gmail.com>
2020-10-18 02:08:32 -0500

    Resolve memory syntax

--- src/make_dataset.sh
+++ src/make_dataset.sh
@@ -2,8 +2,8 @@
 ####################### Batch Headers #########################
 #SBATCH -p Lewis
 #SBATCH -J make_dips_dataset
-#SBATCH -t 0-02:00
-#SBATCH -mem 120G
+#SBATCH -t 0-24:00
+#SBATCH --mem 120G
 #SBATCH -N 1
 #SBATCH -n 24
 ###############################################################
